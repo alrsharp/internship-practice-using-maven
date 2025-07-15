525d0cd0f37ba93e176d078e6ce40912f3bc8337
alrsharp <@flowersseeyourfuture@gmail.com>
2025-07-15 12:21:38 -0700

    added the excel file and checked if it exists

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
@@ -15,6 +17,19 @@ public class Main {
         } catch (Exception e) {
             System.out.println("error: " + e.getMessage());
             //tells if apache poi isn't working
+
         }
+
+        String filePath = "David-Infinity-Purchase-History - Copy.xlsx";
+
+        // tests if the file exists
+        File file = new File(filePath);
+        if (file.exists()) {
+            System.out.println("file found at: " + file.getAbsolutePath());
+        } else {
+            System.out.println("file not found. Looking for: " + file.getAbsolutePath());
+            return; // stops here if file doesn't exist
+        }
+
     }
 }
