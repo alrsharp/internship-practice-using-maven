ab7f74c199035bd11aee15e666e2e8c48c867589
alrsharp <@flowersseeyourfuture@gmail.com>
2025-07-15 12:08:20 -0700

    checks if working correctly

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,13 +6,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
-        // this tests if apach poi is working correctly
+        // this tests if apache poi is working correctly
         try {
             Workbook workbook = new XSSFWorkbook();
+            //framework for an empty excel sheet
             System.out.println("its working correctly");
             workbook.close();
         } catch (Exception e) {
             System.out.println("error: " + e.getMessage());
+            //tells if apache poi isn't working correctly
         }
     }
 }
