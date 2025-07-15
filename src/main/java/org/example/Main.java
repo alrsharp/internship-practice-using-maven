263e978bd0a330b53db982551e7cee920294d673
alrsharp <@flowersseeyourfuture@gmail.com>
2025-07-15 11:49:21 -0700

    fixed import

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,17 @@ package org.example;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        // this tests if apach poi is working correctly
+        try {
+            Workbook workbook = new XSSFWorkbook();
+            System.out.println("its working correctly");
+            workbook.close();
+        } catch (Exception e) {
+            System.out.println("error: " + e.getMessage());
+        }
     }
 }
